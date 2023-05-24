15bd6b3cf02542288a6ec6c10077e075255f0a6f
TheRealYaseen <s.syedyaseen.s@gmail.com>
2023-05-24 23:05:17 +0100

    grid island

--- Graph/graph.js
+++ Graph/graph.js
@@ -184,7 +184,7 @@ function connectedPaths() {
       count++;
     }
   }
-  return count
+  return count;
 }
 
 function traverseConnectedPath(src, visited) {
@@ -196,4 +196,177 @@ function traverseConnectedPath(src, visited) {
   }
 }
 
-console.log(connectedPaths())
+// console.log(connectedPaths())
+
+/*
+Find the largest component
+
+Df through each node in object
+Put nodes visited in a set - to avoid cyclic issues
+Put all nodes part of one traversal in one set or increment a count variable
+This would separate the connected nodes
+*/
+graph = {
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+};
+
+function largestComponent() {
+  let visited = new Set();
+  let numberOfComponents = 0;
+  let components = {};
+  let largestComponent = [];
+  let sizeOfLargestComponent = 0;
+
+  for (const key in graph) {
+    let src = Number(key);
+    let nodesInIteration = 0;
+    if (!visited.has(src)) {
+      numberOfComponents++;
+
+      // traverse(src, visited, (node) => {
+      //   components[numberOfComponents] === undefined
+      //     ? (components[numberOfComponents] = [node])
+      //     : (components[numberOfComponents] = [
+      //         ...components[numberOfComponents],
+      //         node,
+      //       ]);
+      // });
+
+      // This check the largest component every iteration without
+      // trying to do this again separately with another loop
+      // if (largestComponent.length < components[numberOfComponents].length)
+      //   largestComponent = components[numberOfComponents];
+
+      //This only returns #of nodes, not the nodes themselves
+      traverse(src, visited, () => nodesInIteration++);
+      if (sizeOfLargestComponent < nodesInIteration)
+        sizeOfLargestComponent = nodesInIteration;
+    }
+  }
+  // console.log(largestComponent);
+  console.log(sizeOfLargestComponent);
+}
+
+function traverse(src, visited, addToComponents) {
+  if (visited.has(src)) return;
+  visited.add(src);
+  // addToComponents(src);
+  addToComponents();
+  for (const neigh of graph[src]) {
+    traverse(neigh, visited, addToComponents);
+  }
+}
+
+// largestComponent();
+// Result - All components = { 0: [0, 8, 5, 1], 1: [2, 3, 4] };
+// largest component = [ 0, 8, 5, 1 ]
+
+/*
+Find shortest path between two nodes
+Depth first might not work as expected, because the chose direction might left to chance
+Using breadth first would ensure that nodes are visited evenly on each direction --> not left to chance and shortest path is found efficietly
+
+pseudo:
+bf traversal
+terminate if src = dest
+else traverse neightbours and add them to queue
+add visited nodes to set to avoid cyclic issues
+
+increment counter to keep track of distance from source node and send with current node
+return counter once src=dest
+*/
+
+graph = {};
+edgeList = [
+  ["w", "x"],
+  ["x", "y"],
+  ["z", "y"],
+  ["z", "v"],
+  ["w", "v"],
+  ["x", "v"],
+  ["z", "m"],
+  ["m", "y"],
+];
+
+EgdeListToGraph(edgeList);
+// console.log(graph);
+
+function shortestPath(src, dest) {
+  let q = [{ current: src, currentDistance: 0 }];
+  let visited = new Set([src]);
+  let shortestDistance = 0;
+
+  //edge case where src and dest are the same when function is called
+  if (src === dest) return shortestDistance;
+
+  while (q.length > 0) {
+    let { current, currentDistance } = q.shift();
+
+    if (current === dest) {
+      if (shortestDistance === 0) shortestDistance = currentDistance;
+      if (currentDistance < shortestDistance)
+        shortestDistance = currentDistance;
+    }
+
+    for (const neighbour of graph[current]) {
+      if (!visited.has(neighbour)) {
+        visited.add(neighbour);
+        q.push({ current: neighbour, currentDistance: currentDistance + 1 });
+      }
+    }
+  }
+
+  return shortestDistance;
+}
+
+// console.log(shortestPath("x", "m"));
+
+/*
+Island count
+*/
+const grid = [
+  ["W", "L", "W", "W", "W"],
+  ["W", "L", "W", "W", "W"],
+  ["W", "W", "W", "L", "W"],
+  ["W", "W", "L", "L", "W"],
+  ["L", "W", "W", "L", "L"],
+  ["L", "L", "W", "W", "W"],
+];
+
+const islandCount = (grid) => {
+  let visited = new Set()
+
+  let rowLength = grid.length;
+  for (let row = 0; row < rowLength; row++) {
+    let colLength = grid[row].length;
+    for (let column = 0; column < colLength; column++) {
+
+      
+
+
+
+      console.log(grid[row][column]);
+    }
+  }
+};
+
+islandCount(grid);
+// let numberOfComponents = 0;
+// let components = {};
+// function test(node) {
+//   console.log(components[numberOfComponents]);
+//   components[numberOfComponents] === undefined
+//     ? (components[numberOfComponents] = [node])
+//     : (components[numberOfComponents] = [
+//         ...components[numberOfComponents],
+//         node,
+//       ]);
+
+//   numberOfComponents++;
+// }
