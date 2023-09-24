4bbcb065b6938b836cfbb48d4f16d41fe57e3037
TheRealYaseen <s.syedyaseen.s@gmail.com>
2023-09-24 14:50:28 +0100

    14E complete

--- Leetcode/14ELongestCommonPrefix.py
+++ Leetcode/14ELongestCommonPrefix.py
@@ -1,11 +1,28 @@
-
 def longestCommonPrefix(strs):
-    index = 0
-    for i in strs:
-        print(i)
+    if len(strs) == 0:
+        return ""
+
+    largestIndex = len(strs[0]) - 1
+
+    result = strs[0]
+    for word in strs[1:]:
+        if len(word) < len(result):
+            largestIndex = len(word) - 1
+            result = result[0:largestIndex + 1]
+
+        for j in range(largestIndex):
+            if result[j] != word[j]:
+                largestIndex = j
+                if largestIndex == 0:
+                    return ""
+                result = result[0:j]
+
+    return result
 
 
-longestCommonPrefix(["flower", "flow", "flight"])
+print(longestCommonPrefix(["flower", "flow", "flight"]))
+print(longestCommonPrefix(["ab", "a"]))
+
 
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
